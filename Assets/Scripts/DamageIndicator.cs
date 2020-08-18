8785cc3e4d660fe65a4878a927521665a9dafd37
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-08-17 23:40:48 -0300

    Implementing basic hit compass when player is hit

---
+++ Assets/Scripts/DamageIndicator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MizJam
+{
+    public class DamageIndicator : MonoBehaviour
+    {
+        private Enemy enemy;
+        private Image indicatorImage;
+
+        void Start()
+        {
+            indicatorImage = GetComponentInChildren<Image>();
+            Destroy(this.gameObject, 2f);
+        }
+
+        void Update()
+        {
+            var dirVector = enemy.transform.position - Camera.main.transform.parent.position;
+            dirVector.y = 0;
+            var rot = Quaternion.FromToRotation(Camera.main.transform.parent.forward, dirVector);
+            float angle = rot.eulerAngles.y;
+            indicatorImage.transform.parent.eulerAngles = new Vector3(0, 0, -angle);
+        }
+
+        public void SetDamageSource(Enemy enemy)
+        {
+            this.enemy = enemy;
+        }
+    }
+}
---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MizJam
+{
+    public class HUDManager : MonoBehaviour
+    {
+        public static HUDManager instance;
+
+        [SerializeField]
+        private GameObject dmgIndicationCanvas, dmgIndicatorPrefab;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+        }
+
+        public void DisplayHitDirection(Enemy enemy)
+        {
+            DamageIndicator di = Instantiate(dmgIndicatorPrefab, dmgIndicationCanvas.transform).GetComponent<DamageIndicator>();
+            di.SetDamageSource(enemy);
+        }
+    }
+}
