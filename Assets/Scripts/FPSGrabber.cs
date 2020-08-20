d5a6a5656128dc959935dc6adc84b02ad2b12f08
Luca <lams3@cin.ufpe.br>
2020-08-20 00:26:10 -0300

    First Version of a Throwable object

---
+++ Assets/Scripts/FPSGrabber.cs
@@ -0,0 +1,55 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace MizJam
+{
+    public class FPSGrabber : MonoBehaviour
+    {
+        [SerializeField]
+        private float grabDistance, throwForce;
+
+        [SerializeField]
+        private LayerMask grabbable;
+
+        [SerializeField]
+        private Transform holdPosition;
+
+        [SerializeField]
+        private new Camera camera;
+
+        private Grabbable grabbed;
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                if(this.grabbed)
+                    this.Throw();
+                else
+                    this.TryGrab();
+            }
+        }
+
+        private void TryGrab()
+        {
+            Ray ray = new Ray(this.camera.transform.position, this.camera.transform.forward);
+            if (Physics.Raycast(ray, out RaycastHit hit, this.grabDistance, this.grabbable))
+                this.Grab(hit.collider.GetComponentInParent<Grabbable>());
+        }
+
+        private void Grab(Grabbable toGrab)
+        {
+            toGrab.GetGrabbed();
+
+            this.grabbed = toGrab;
+            toGrab.transform.SetParent(this.holdPosition);
+            toGrab.transform.DOLocalMove(Vector3.zero, 0.5f);
+        }
+        private void Throw()
+        {
+            this.grabbed.transform.SetParent(null);
+            this.grabbed.GetThrown(this.throwForce * this.transform.forward);
+            this.grabbed = null;
+        }
+    }
+}
---
+++ Assets/Scripts/Grabbable.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace MizJam
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Grabbable : MonoBehaviour
+    {
+        [SerializeField]
+        private float explosionRadius = 5.0f;
+
+        [SerializeField]
+        private LayerMask enemies;
+
+        private new Rigidbody rigidbody;
+        private bool beeingThrown = false;
+
+
+        private void Update()
+        {
+            this.beeingThrown = this.rigidbody.velocity.magnitude >= 1.0f;
+        }
+
+        private void Awake()
+        {
+            this.rigidbody = this.GetComponent<Rigidbody>(); 
+        }
+
+        public void GetGrabbed()
+        {
+            this.rigidbody.detectCollisions = false;
+            this.rigidbody.isKinematic = true;
+        }
+
+        public void GetThrown(Vector3 force)
+        {
+            this.rigidbody.isKinematic = false;
+            this.rigidbody.detectCollisions = true;
+            this.rigidbody.AddForce(force, ForceMode.Impulse);
+            this.beeingThrown = true;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (this.beeingThrown)
+            {
+                IEnumerable<Enemy> inRange = Physics.OverlapSphere(this.transform.position, this.explosionRadius, this.enemies).Select(el => el.GetComponent<Enemy>()).Where(el => el != null);
+                foreach (Enemy enemy in inRange)
+                    enemy.SufferImpact(this.transform.position);
+            }
+        }
+    }
+}
