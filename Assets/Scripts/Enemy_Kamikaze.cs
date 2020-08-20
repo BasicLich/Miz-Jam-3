ab97ef7436f2b0165cb05d55e25736e63c212e20
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-08-19 23:21:20 -0300

    Adding player detection logic on Kamikaze enemy

---
+++ Assets/Scripts/Enemy_Kamikaze.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Enemy_Kamikaze : MonoBehaviour
+{
+    private Animator animator;
+
+    private bool isChasingPlayer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (!isChasingPlayer)
+        {
+            if (SeesPlayer())
+            {
+                isChasingPlayer = true;
+                Debug.Log("Eita Porra, vi hein!");
+                //start chase
+            }
+        }
+        else
+        {
+            //check for explosion
+        }
+    }
+
+    private bool SeesPlayer()
+    {
+        RaycastHit raycastHit;
+        Physics.Linecast(transform.position, Camera.main.transform.position, out raycastHit);
+        return (raycastHit.transform.root.tag == "Player");
+    }
+
+    private void StartChase()
+    {
+
+    }
+}
