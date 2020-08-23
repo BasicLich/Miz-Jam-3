a943be409f8342324ccd3b63e80d64668f66b5b5
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-08-22 22:50:45 -0300

    Improvements on the boss and adding TMP to project

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -10,6 +10,18 @@ namespace MizJam
         [SerializeField]
         private Slider healthBar;
 
+        [Header("RangedAttack")]
+        [SerializeField]
+        private GameObject normalProjectilePrefab;
+        [SerializeField]
+        private GameObject enragedProjectilePrefab;
+
+        [Header("Summon")]
+        [SerializeField]
+        private GameObject[] minionsPrefabs;
+        [SerializeField]
+        private GameObject summonFXPrefab;
+
         public float health;
         private float initialHealth;
         private Animator animator;
@@ -32,7 +44,7 @@ namespace MizJam
             health -= damage;
             healthBar.value = health / initialHealth;
 
-            if (health < initialHealth / 2)
+            if (health <= initialHealth / 2)
             {
                 animator.SetBool("isEnraged", true);
             }
@@ -51,6 +63,27 @@ namespace MizJam
         private void Die()
         {
             animator.SetTrigger("DidDie");
+        }
+
+        public void ThrowProjectileAtPlayer()
+        {
+            GameObject projectilePrefab = animator.GetBool("isEnraged") ? enragedProjectilePrefab : normalProjectilePrefab;
+
+            Vector3 dir = (Camera.main.transform.position - transform.position).normalized;
+            GameObject projectile = Instantiate(projectilePrefab);
+            projectile.transform.position = transform.Find("Body").Find("Bow").position;
+            //projectile.GetComponent<Projectile>().SetSource(this);
+            projectile.GetComponent<Rigidbody>().AddForceAtPosition(dir * 3000, Vector3.zero);
+        }
+
+        public void SpawnMinion()
+        {
+            int rand = Random.Range(0, minionsPrefabs.Length);
+            GameObject minion = Instantiate(minionsPrefabs[rand]);
+
+            Vector3 summonPos = animator.transform.position + animator.transform.Find("Body").forward * 6;
+            minion.transform.position = summonPos;
+            Instantiate(summonFXPrefab).transform.position = summonPos;
         }
     }
 }
